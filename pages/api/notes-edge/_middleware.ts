fc176c3845805baea80530988a4dd4889791ba31
React Mastering <pga.adam.scott@gmail.com>
2022-06-05 16:03:22 +0900

    fixed the middleware

--- pages/api/notes-edge/_middleware.ts
+++ pages/api/notes-edge/_middleware.ts
@@ -1,12 +1,25 @@
 import { NextRequest, NextResponse } from 'next/server'
-import { supabase } from '../../../utils/supabase'
+//import { supabase } from '../../../utils/supabase'
 
 export const middleware = async (req: NextRequest, res: NextResponse) => {
-  const { data, error } = await supabase
-    .from('notes')
-    .select('*')
-    .order('created_at', { ascending: true })
+  //Original
+  // const { data, error } = await supabase
+  //   .from('notes')
+  //   .select('*')
+  //   .order('created_at', { ascending: true })
 
-  if (error) return NextResponse.json({ message: 'Fetch error' })
-  return NextResponse.json(data)
+  // if (error) return NextResponse.json({ message: 'Fetch error' })
+  // return NextResponse.json(data)
+
+  //After modification to direct fetch to REST API end point (Supabase)
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_SUPABASE_URL}/rest/v1/notes?select=*`,
+    {
+      headers: {
+        apikey: process.env.NEXT_PUBLIC_SUPABASE_API_KEY!,
+      },
+    }
+  )
+  const notes = await response.json()
+  return NextResponse.json(notes)
 }
